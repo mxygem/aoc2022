200e9b5d81f562f9faf7ffc8e89d125409596c1f
mxygem <gh@nes.33mail.com>
2022-12-02 00:09:35 -0700

    Wrap day 1 golfed version for "readability"

--- go/day-01/01-g/main.go
+++ go/day-01/01-g/main.go
@@ -1,6 +1,9 @@
-package main;import(."bytes";."log";."os";."sort";."strconv");func main(){f,_:=ReadFile(Args[2]);e:=[]int{0};for _,s:=range Split(f,[]byte("\n")){if len(s)==0{e=append(e,0);continue};x,_:=Atoi(string(s));e[len(e)-1]+=x};Ints(e);l:=len(e)-1;Printf("%d:%d",e[l],e[l]+e[l-1]+e[l-2])}
-
 // golfed: 276 characters
+
+package main;import(."bytes";."log";."os";."sort";."strconv");func main(){f,_:=
+ReadFile(Args[2]);e:=[]int{0};for _,s:=range Split(f,[]byte("\n")){if len(s)==0{
+e=append(e,0);continue};x,_:=Atoi(string(s));e[len(e)-1]+=x};Ints(e);l:=len(e)-1;
+Printf("%d:%d",e[l],e[l]+e[l-1]+e[l-2])}
 
 // un-golfed: 350
 
